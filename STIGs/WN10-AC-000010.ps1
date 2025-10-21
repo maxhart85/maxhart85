0a4ebfc5093b4747bb15fcf314bcb429fb4b5551
Graham Max Hart <39869247+maxhart85@users.noreply.github.com>
2025-10-21 18:42:05 -0500

    WN10-AC-000010.ps1

--- STIGs/WN10-AC-000010.ps1
+++ STIGs/WN10-AC-000010.ps1
@@ -1,4 +1,26 @@
-# WN10-AC-000010: Account lockout threshold should be 3 or less (not 0)
+<#
+.SYNOPSIS
+    This PowerShell script sets the number of allowed failed login attempts to 3.
+
+.NOTES
+    Author          : Graham Hart
+    LinkedIn        : linkedin.com/in/graham-hart-5b0123162/
+    GitHub          : github.com/maxhart85
+    Date Created    : 2025-10-21
+    Last Modified   : 2025-10-21
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AC-000010
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+
+#># WN10-AC-000010: Account lockout threshold should be 3 or less (not 0)
 # Run as Administrator
 
 # Step 1: Check current lockout threshold
